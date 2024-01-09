76bd03b9b556a84417fc81c6051698a97401d9bc
nclinco <nicoclinco@gmail.com>
2024-01-09 23:39:06 +0100

    Added new stuffs

--- homework_02.01/setup.py
+++ homework_02.01/setup.py
@@ -9,34 +9,72 @@ class get_pybind_include(object):
         import pybind11
         return pybind11.get_include()
 
-extra_compile_args_df =['--std=c++17'];
-extra_compile_args_q  =['--std=c++17','-lgsl','-lgslcblas'];
 
-EIGEN_PATH = '/home/nico/Desktop/PHD-COURSES/libraries/eigen/include/eigen3'
-GSL_PATH = '/home/nico/Desktop/PHD-COURSES/libraries/gsl'+'/include'
-BOOST_PATH= '/home/nico/Desktop/PHD-COURSES/libraries/Boost'+'/include'
+extra_compile_args_df =['--std=c++17','-Wall','-Werror'];
 
-dataframe_path = '/home/nico/Desktop/PHD-COURSES/ADVANCED_PROGRAMMING/homework_02.01/src/data_frame_module'
-df_bind_path='/home/nico/Desktop/PHD-COURSES/ADVANCED_PROGRAMMING/homework_02.01/src/pyDirDF'
-quad_path = '/home/nico/Desktop/PHD-COURSES/ADVANCED_PROGRAMMING/homework_02.01/src/quad_module'
-q_bind_path='/home/nico/Desktop/PHD-COURSES/ADVANCED_PROGRAMMING/homework_02.01/src/pyDirQuad'
+extra_compile_args_q  =['--std=c++17','-Wall','-Werror'];
 
-headers_dir='/home/nico/Desktop/PHD-COURSES/ADVANCED_PROGRAMMING/homework_02.01/Headers'
+EIGEN_PATH = '/home/nico/Desktop/PHD-COURSES/libraries/eigen';
+GSL_PATH = '/home/nico/Desktop/PHD-COURSES/libraries/gsl';
+BOOST_PATH = '/home/nico/Desktop/PHD-COURSES/libraries/Boost';
+
+EIGEN_PATH_INCLUDE = EIGEN_PATH +'/include/eigen3'
+GSL_PATH_INCLUDE = GSL_PATH +'/include'
+BOOST_PATH_INCLUDE = BOOST_PATH+'/include'
+
+import os;
+cwd = os.getcwd(); #Current work directory
+
+#Path for C++ libraries:
+dataframe_path = cwd+'/src/data_frame_module';
+quad_path = cwd+'/src/quad_module';
+
+df_bind_path = cwd+'/src/pyDirDF';
+q_bind_path=cwd+'/src/pyDirQuad';
+
+#directory of headers:
+headers_dir=cwd+'/Headers'
 
 extra_link_args_qb = [];
-extra_link_args_qb.append('-Wl,-rpath,' + '/home/nico/Desktop/PHD-COURSES/libraries/gsl'+'/lib')
-extra_link_args_qb.append('-L'+'/home/nico/Desktop/PHD-COURSES/libraries/gsl'+'/lib')
+extra_link_args_qb.append('-Wl,-rpath,' + GSL_PATH+'/lib');
+extra_link_args_qb.append('-L'+GSL_PATH+'/lib');
 extra_link_args_qb.append('-lgsl');
 extra_link_args_qb.append('-lgslcblas');
 
+
+Extension_statistics = Extension(                                                                \
+        'dataFrameBind',                                                                         \
+        [dataframe_path+'/DATA_FRAME.cpp',df_bind_path+'/dataFrameBind.cpp'],                    \
+        include_dirs=[                                                                           \
+        get_pybind_include(), \
+        headers_dir,          \
+        BOOST_PATH_INCLUDE,   \
+        ],                    \
+        language='c++',       \
+        extra_compile_args=extra_compile_args_df);
+
+Extension_quadrature =     Extension(  \
+                           'quadBind', \
+                           [quad_path+'/Quadrature.cpp',q_bind_path+'/quadBind.cpp'], \
+                           library_dirs=[GSL_PATH+'/lib'],  \
+                           include_dirs=[ \
+                           get_pybind_include(), \
+                           headers_dir, \
+                           GSL_PATH_INCLUDE, \
+                           ], \
+                           language='c++', \
+                           extra_compile_args=extra_compile_args_q, \
+                           extra_link_args=extra_link_args_qb);
+
+"""
 ext_modules = [
     Extension(
-        'dataFrameBind', # Name of the module.
+        'dataFrameBind',          # Name of the module.
         [dataframe_path+'/DATA_FRAME.cpp',df_bind_path+'/dataFrameBind.cpp'], # C++ source files.
         include_dirs=[
             get_pybind_include(), # Path to pybind11 includes.
-            headers_dir, # Additional include paths.
-            BOOST_PATH,
+            headers_dir,          # Additional include paths.
+            BOOST_PATH_INCLUDE,
         ],
         language='c++',
         extra_compile_args=extra_compile_args_df
@@ -44,26 +82,51 @@ ext_modules = [
     Extension(
         'quadBind',
         [quad_path+'/Quadrature.cpp',q_bind_path+'/quadBind.cpp'],
-        library_dirs=['/home/nico/Desktop/PHD-COURSES/libraries/gsl/lib'],
+        library_dirs=[GSL_PATH+'/lib'],
         include_dirs=[
             get_pybind_include(),
             headers_dir,
-            GSL_PATH,
+            GSL_PATH_INCLUDE,
             ],
         language='c++',
         extra_link_args=extra_link_args_qb
         )
 ]
+"""
+ext_modules_statistics = [
+    Extension_statistics
+];
 
-setup(
-name='pyBinding',
-version='0.0',
-author='Nicola Clinco',
-author_email='nicoclinco@gmail.com',
-description='Creating the libraries for statistic and quadrature module',
-long_description='',
-ext_modules=ext_modules,
-install_requires=['pybind11>=2.5.0'], # pybind11 requirement.
-cmdclass={'build_ext': build_ext},
-zip_safe=False,
-)
+ext_modules_quadrature = [
+    Extension_quadrature
+];
+
+build_statistics=True;
+build_quadrature=True;
+
+if(build_statistics):
+    setup(
+    name='pyDataFrame',
+    version='1.0',
+    author='Nicola Clinco',
+    author_email='nicoclinco@gmail.com',
+    description='Library for the statistics module',
+    long_description='',
+    ext_modules=ext_modules_statistics,
+    install_requires=['pybind11>=2.5.0'], # pybind11 requirement.
+    cmdclass={'build_ext': build_ext},
+    zip_safe=False,
+    )
+if(build_quadrature):
+    setup(
+    name='pyQuadrature',
+    version='1.0',
+    author='Nicola Clinco',
+    author_email='nicoclinco@gmail.com',
+    description='Library for the quadrature module',
+    long_description='',
+    ext_modules=ext_modules_quadrature,
+    install_requires=['pybind11>=2.5.0'], # pybind11 requirement.
+    cmdclass={'build_ext': build_ext},
+    zip_safe=False,
+    )
