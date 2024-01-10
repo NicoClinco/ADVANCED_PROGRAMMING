01d1a492f132ccdf9d314198ada12fda519bfcb6
nclinco <nicoclinco@gmail.com>
2024-01-10 13:16:34 +0100

    Fixed test and setup.py

--- homework_02.01/setup.py
+++ homework_02.01/setup.py
@@ -42,16 +42,16 @@ extra_link_args_qb.append('-lgsl');
 extra_link_args_qb.append('-lgslcblas');
 
 
-Extension_statistics = Extension(                                                                \
-        'dataFrameBind',                                                                         \
-        [dataframe_path+'/DATA_FRAME.cpp',df_bind_path+'/dataFrameBind.cpp'],                    \
-        include_dirs=[                                                                           \
-        get_pybind_include(), \
-        headers_dir,          \
-        BOOST_PATH_INCLUDE,   \
-        ],                    \
-        language='c++',       \
-        extra_compile_args=extra_compile_args_df);
+Extension_statistics =  Extension( \
+                        'dataFrameBind', \
+                        [dataframe_path+'/DATA_FRAME.cpp',df_bind_path+'/dataFrameBind.cpp'],  \
+                        include_dirs=[                                                         \
+                        get_pybind_include(), \
+                        headers_dir,          \
+                        BOOST_PATH_INCLUDE,   \
+                        ],                    \
+                        language='c++',       \
+                        extra_compile_args=extra_compile_args_df);
 
 Extension_quadrature =     Extension(  \
                            'quadBind', \
@@ -66,67 +66,37 @@ Extension_quadrature =     Extension(  \
                            extra_compile_args=extra_compile_args_q, \
                            extra_link_args=extra_link_args_qb);
 
-"""
-ext_modules = [
-    Extension(
-        'dataFrameBind',          # Name of the module.
-        [dataframe_path+'/DATA_FRAME.cpp',df_bind_path+'/dataFrameBind.cpp'], # C++ source files.
-        include_dirs=[
-            get_pybind_include(), # Path to pybind11 includes.
-            headers_dir,          # Additional include paths.
-            BOOST_PATH_INCLUDE,
-        ],
-        language='c++',
-        extra_compile_args=extra_compile_args_df
-    ),
-    Extension(
-        'quadBind',
-        [quad_path+'/Quadrature.cpp',q_bind_path+'/quadBind.cpp'],
-        library_dirs=[GSL_PATH+'/lib'],
-        include_dirs=[
-            get_pybind_include(),
-            headers_dir,
-            GSL_PATH_INCLUDE,
-            ],
-        language='c++',
-        extra_link_args=extra_link_args_qb
-        )
-]
-"""
-ext_modules_statistics = [
-    Extension_statistics
-];
-
-ext_modules_quadrature = [
-    Extension_quadrature
-];
+#Specify here if you want only the statistics or the quadrature module:
 
 build_statistics=True;
 build_quadrature=True;
 
+ext_modules= [];
+
 if(build_statistics):
-    setup(
-    name='pyDataFrame',
-    version='1.0',
-    author='Nicola Clinco',
-    author_email='nicoclinco@gmail.com',
-    description='Library for the statistics module',
-    long_description='',
-    ext_modules=ext_modules_statistics,
-    install_requires=['pybind11>=2.5.0'], # pybind11 requirement.
-    cmdclass={'build_ext': build_ext},
-    zip_safe=False,
-    )
+    ext_modules.append(Extension_statistics);
 if(build_quadrature):
-    setup(
-    name='pyQuadrature',
-    version='1.0',
-    author='Nicola Clinco',
-    author_email='nicoclinco@gmail.com',
-    description='Library for the quadrature module',
-    long_description='',
-    ext_modules=ext_modules_quadrature,
-    install_requires=['pybind11>=2.5.0'], # pybind11 requirement.
-    cmdclass={'build_ext': build_ext},
-    zip_safe=False,
-    )
+    ext_modules.append(Extension_quadrature);
+
+if(build_statistics and build_quadrature):
+    name = 'pyGeneral';
+elif(build_statistics):
+    name = 'pyDataFrame';
+elif(build_quadrature):
+    name = 'pyQuadrature';
+else:
+    name = 'default';
+
+setup(
+name='pyDataFrame',
+version='1.0',
+author='Nicola Clinco',
+author_email='nicoclinco@gmail.com',
+description='Library for statistics and quadrature module',
+long_description='',
+ext_modules=ext_modules,
+install_requires=['pybind11>=2.5.0'], # pybind11 requirement.
+cmdclass={'build_ext': build_ext},
+zip_safe=False,
+);
+
